ec83c3c8c173afabca3dd0157ac8e6566bab7a34
Zheng Luo <helloworld09@163.com>
2018-05-06 16:40:59 +0800

    Fix bug

--- bootstrap/iterate.py
+++ bootstrap/iterate.py
@@ -4,8 +4,8 @@ from bootstrap.load_data import init_word2idx, init_sequences
 
 attr_word2idx, value_word2idx = init_word2idx()
 all_data = init_sequences('all_data.json', attr_word2idx, value_word2idx)
-X = all_data[:2]
-y = all_data[2:]
+X = list(all_data[:2])
+y = list(all_data[2:])
 
 model = load_model('model.h5')
 y_predict = model.predict(X, verbose=1)
